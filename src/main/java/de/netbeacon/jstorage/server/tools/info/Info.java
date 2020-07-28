afade4af44a776d9364597968c2c440b3c8596b3
Horstexplorer <21320859+Horstexplorer@users.noreply.github.com>
2020-07-28 02:37:34 +0200

    updated version

--- src/main/java/de/netbeacon/jstorage/server/tools/info/Info.java
+++ src/main/java/de/netbeacon/jstorage/server/tools/info/Info.java
@@ -23,6 +23,6 @@ package de.netbeacon.jstorage.server.tools.info;
  */
 public class Info {
 
-    public static final String VERSION = "1.6.0_24";
+    public static final String VERSION = "1.7.0_25";
 
 }
